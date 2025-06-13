3ccec1123b53ba633c3c75b77e0ce3ae6c8c5bfb
Ashot <adodus13@gmail.com>
2025-06-13 15:49:03 +0300

    Fix: Models for migrations

--- app/Models/PostTag.php
+++ app/Models/PostTag.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PostTag extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'post_tags';
+    protected $guarded = false;
 }
