cd045fae214617789401e5dd2fe53132460792ab
Ashot <adodus13@gmail.com>
2025-06-13 18:13:03 +0300

    Add: admin view and Fix: for project

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,12 @@ Route::group(['namespace' => 'Main'], function () {
     Route::get('/', [IndexController::class, '__invoke']);
 });
 
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', [App\Http\Controllers\Admin\Main\IndexController::class, '__invoke']);
+    });
+});
+
 Auth::routes();
 
 
